d282dea3df146a1db4bd93f9a67c7b970862f6a6
Yue Ying <yying7@jh.edu>
2026-06-09 13:38:33 -0400

    Extract 2D reaction table spec

--- src/reactions/create_DDMatrices.cpp
+++ src/reactions/create_DDMatrices.cpp
@@ -1,20 +1,23 @@
 #include "reactions/bimolecular/2D_reaction_table_functions.hpp"
+#include "reactions/bimolecular/probability_engine.hpp"
 
-void create_DDMatrices(gsl_matrix*& survMatrix, gsl_matrix*& normMatrix, gsl_matrix*& pirMatrix, double bindRadius,
-    double Dtot, double comRMax, double ktemp, const Parameters& params)
-{
-    //    char fname[100];
-    double RStepSize { sqrt(Dtot * params.timeStep) / 50 };
+void create_DDMatrices(gsl_matrix*& survMatrix, gsl_matrix*& normMatrix, gsl_matrix*& pirMatrix,
+                       double bindRadius, double Dtot, double comRMax, double ktemp,
+                       const Parameters& params) {
+  //    char fname[100];
+  const nerdss::probability::TwoDReactionTableSpec spec{bindRadius, Dtot, comRMax, ktemp,
+                                                        params.timeStep};
+  double RStepSize{nerdss::probability::two_d_table_step_size(spec)};
 
-    create_normMatrix(normMatrix, bindRadius, Dtot, ktemp, comRMax, RStepSize, params);
-    // std::cout << "Norm matrix successfully created for Dtot: " << Dtot << " timeStep "
-    //           << ", bindRadius: " << bindRadius << '\n';
+  create_normMatrix(normMatrix, bindRadius, Dtot, ktemp, comRMax, RStepSize, params);
+  // std::cout << "Norm matrix successfully created for Dtot: " << Dtot << " timeStep "
+  //           << ", bindRadius: " << bindRadius << '\n';
 
-    create_survMatrix(survMatrix, bindRadius, Dtot, ktemp, comRMax, RStepSize, params);
-    // std::cout << "Survival matrix successfully created for Dtot: " << Dtot << " timeStep "
-    //           << ", bindRadius: " << bindRadius << '\n';
+  create_survMatrix(survMatrix, bindRadius, Dtot, ktemp, comRMax, RStepSize, params);
+  // std::cout << "Survival matrix successfully created for Dtot: " << Dtot << " timeStep "
+  //           << ", bindRadius: " << bindRadius << '\n';
 
-    create_pirMatrix(pirMatrix, bindRadius, Dtot, ktemp, comRMax, RStepSize, params);
-    // std::cout << "Pir matrix successfully created for Dtot: " << Dtot << " timeStep "
-    //           << ", bindRadius: " << bindRadius << '\n';
+  create_pirMatrix(pirMatrix, bindRadius, Dtot, ktemp, comRMax, RStepSize, params);
+  // std::cout << "Pir matrix successfully created for Dtot: " << Dtot << " timeStep "
+  //           << ", bindRadius: " << bindRadius << '\n';
 }
--- src/reactions/size_lookup.cpp
+++ src/reactions/size_lookup.cpp
@@ -1,15 +1,8 @@
 #include "reactions/bimolecular/2D_reaction_table_functions.hpp"
+#include "reactions/bimolecular/probability_engine.hpp"
 
-size_t size_lookup(double bindRadius, double Dtot, const Parameters& params, double Rmax)
-{
-    int ctr{ 0 };
-    double stepSize {sqrt(Dtot * params.timeStep) / 50};
-
-    double RIndex{ bindRadius };
-    while (RIndex <= Rmax + stepSize) {
-        ctr += 1;
-        RIndex += stepSize;
-    }
-
-    return static_cast<size_t>(ctr);
+size_t size_lookup(double bindRadius, double Dtot, const Parameters& params, double Rmax) {
+  const nerdss::probability::TwoDReactionTableSpec spec{bindRadius, Dtot, Rmax, 0.0,
+                                                        params.timeStep};
+  return nerdss::probability::two_d_table_size(spec);
 }
